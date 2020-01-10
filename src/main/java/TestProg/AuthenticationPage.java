eb48bca71c8b2b8524b1d779f26426c2d3a14bfe
RenRevan <funnyincident28@gmail.com>
2020-01-10 23:54:45 +0200

    add builder

--- src/main/java/TestProg/AuthenticationPage.java
+++ src/main/java/TestProg/AuthenticationPage.java
@@ -8,13 +8,41 @@ public class AuthenticationPage {
 
     private static final String AUTHENTICATION_PAGE_URL = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
     private WebDriver driver;
+    private static By authCreateEmail = By.id("email_create");
+    private static By authCreateSubmit = By.id("SubmitCreate");
+    private static  By PERS_INF_FIRST_NAME = By.id("customer_firstname");
+    private static  By PERS_INF_LAST_NAME = By.id("customer_lastname");
+    private static  By PERS_INF_PASSWORD = By.id("passwd");
+    private static  By ADDRESS_FIRST_NAMED = By.id("firstname");
+    private static  By ADDRESS_LAST_NAME = By.id("lastname");
+    private static  By ADDRESS_ADDRESS = By.id("address1");
+    private static  By ADDRESS_CITY = By.id("city");
+    private static  By ADDRESS_POSTAL_CODE = By.id("postcode");
+    private static  By ADDRESS_MOBILR_PHONE= By.id("phone_mobile");
+    private static  By ADDRESS_ALIAS_ADDRESS= By.id("alias");
+    private static  By ADDRESS_REGISTER= By.id("submitAccount");
+    private static  By ALLERT = By.xpath("//*[@class='alert alert-danger']");
 
 
 
 
-    public AuthenticationPage(WebDriver driver){
-        this.driver = driver;
+//    public AuthenticationPage(WebDriver driver){
+////        this.driver = driver;
+////    }
+    private AuthenticationPage(Builder builder) {
+        this.driver = builder.driver;
+        enterValue(PERS_INF_FIRST_NAME,builder.persInfoFirstName);
+        enterValue(PERS_INF_LAST_NAME,builder.persInfoLastName);
+        enterValue(PERS_INF_PASSWORD,builder.persInfoPassword);
+        enterValue(ADDRESS_FIRST_NAMED,builder.AddressFirstName);
+        enterValue(ADDRESS_LAST_NAME,builder.AddressLastName);
+        enterValue(ADDRESS_ADDRESS,builder.AddressAdress);
+        enterValue(ADDRESS_CITY,builder.AddressCity);
+        enterValue(ADDRESS_POSTAL_CODE,builder.AddressPostalCode);
+        enterValue(ADDRESS_MOBILR_PHONE,builder.AddressMobilePhone);
+        enterValue(ADDRESS_ALIAS_ADDRESS,builder.AddressAliasAddress);
     }
+
 
     public AuthenticationPage openPage(){
         this.driver.navigate().to(AUTHENTICATION_PAGE_URL);
@@ -32,7 +60,63 @@ public class AuthenticationPage {
         return this;
     }
 
+public  static  class Builder {
+    // pers info Parameters
+    private final WebDriver driver;
+    private final String persInfoFirstName;
+    private final String persInfoLastName ;
+    private final String persInfoPassword ;
+    // al info parameters
+    private  String AddressFirstName ;
+    private  String AddressLastName ;
+    private  String AddressAdress ;
+    private  String AddressCity ;
+    private  String AddressPostalCode;
+    private  String AddressMobilePhone;
+    private  String AddressAliasAddress;
 
+public  Builder(WebDriver driver, String FirstName, String LastName, String password){
+    this.driver = driver;
+    this.persInfoFirstName=FirstName;
+   this.persInfoLastName=LastName;
+   this.persInfoPassword=password;
+}
+
+public Builder setAddressFirstName(String value){
+    this.AddressFirstName=value;
+    return this;
+}
+    public Builder setAddressLastName(String value){
+        this.AddressLastName=value;
+        return this;
+    }
+    public Builder setAddressAdress(String value){
+        this.AddressAdress=value;
+        return this;
+    }
+    public Builder setAddressCity(String value){
+        this.AddressCity=value;
+        return this;
+    }
+    public Builder setAddressPostalCode(String value){
+        this.AddressPostalCode=value;
+        return this;
+    }
+    public Builder setAddressMobilePhone(String value){
+        this.AddressMobilePhone=value;
+        return this;
+    }
+    public Builder setAddressAliasAddress(String value){
+        this.AddressAliasAddress=value;
+        return this;
+    }
+
+    public AuthenticationPage build() {
+        return new AuthenticationPage(this);
+    }
+
+
+    }
 
 
 
--- src/test/java/TestProg/AuthenticationCreate.java
+++ src/test/java/TestProg/AuthenticationCreate.java
@@ -9,8 +9,6 @@ import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
-import java.time.Instant;
 import java.util.concurrent.TimeUnit;
 
 public class AuthenticationCreate {
@@ -19,26 +17,24 @@ public class AuthenticationCreate {
     private static By authCreateEmail = By.id("email_create");
     private static By authCreateSubmit = By.id("SubmitCreate");
     private static  By PERS_INF_FIRST_NAME = By.id("customer_firstname");
-    private static  By PERS_INF_LAST_NAME = By.id("customer_lastname");
-    private static  By PERS_INF_PASSWORD = By.id("passwd");
-    private static  By ADDRESS_FIRST_NAMED = By.id("firstname");
-    private static  By ADDRESS_LAST_NAME = By.id("lastname");
-    private static  By ADDRESS_ADDRESS = By.id("address1");
-    private static  By ADDRESS_CITY = By.id("city");
-    private static  By ADDRESS_POSTAL_CODE = By.id("postcode");
-    private static  By ADDRESS_MOBILR_PHONE= By.id("phone_mobile");
-    private static  By ADDRESS_ALIAS_ADDRESS= By.id("alias");
     private static  By ADDRESS_REGISTER= By.id("submitAccount");
     private static  By ALLERT = By.xpath("//*[@class='alert alert-danger']");
 
 
     @BeforeTest
     public static void runDriver() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ирен\\IdeaProjects\\l_SelDriver\\src\\main\\resources\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "D:\\drivers\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().pageLoadTimeout(50, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        authPage = new AuthenticationPage(driver);
+        authPage = new AuthenticationPage.Builder(driver,"PERS_INF_FIRST_NAME", "PERS_INF_LAST_NAME","12345678")
+                .setAddressFirstName("ADDRESS_FIRST_NAMED")
+                .setAddressLastName("ADDRESS_LAST_NAME")
+                .setAddressAdress("64/13, Volodymyrska Street")
+                .setAddressCity("Kyiv")
+                .setAddressPostalCode("01601")
+                .setAddressMobilePhone("+80934655725")
+                .setAddressAliasAddress("64/13, Volodymyrska Street").build();
 
     }
 
@@ -52,17 +48,18 @@ public class AuthenticationCreate {
     }
     @Test
     public static void enterPersonalInfo(){
-        authPage.enterValue(PERS_INF_FIRST_NAME,"PERS_INF_FIRST_NAME")
-                .enterValue(PERS_INF_LAST_NAME,"PERS_INF_LAST_NAME")
-                .enterValue(PERS_INF_PASSWORD,"12345678")
-                .enterValue(ADDRESS_FIRST_NAMED,"ADDRESS_FIRST_NAMED")
-                .enterValue(ADDRESS_LAST_NAME,"ADDRESS_LAST_NAME")
-                .enterValue(ADDRESS_ADDRESS,"64/13, Volodymyrska Street")
-                .enterValue(ADDRESS_CITY,"Kyiv")
-                .enterValue(ADDRESS_POSTAL_CODE,"01601")
-                .enterValue(ADDRESS_MOBILR_PHONE,"+80934655725")
-                .enterValue(ADDRESS_ALIAS_ADDRESS,"64/13, Volodymyrska Street")
-                .clickSubmitButton(ADDRESS_REGISTER);
+//        authPage.enterValue(PERS_INF_FIRST_NAME,"PERS_INF_FIRST_NAME")
+//                .enterValue(PERS_INF_LAST_NAME,"PERS_INF_LAST_NAME")
+//                .enterValue(PERS_INF_PASSWORD,"12345678")
+//                .enterValue(ADDRESS_FIRST_NAMED,"ADDRESS_FIRST_NAMED")
+//                .enterValue(ADDRESS_LAST_NAME,"ADDRESS_LAST_NAME")
+//                .enterValue(ADDRESS_ADDRESS,"64/13, Volodymyrska Street")
+//                .enterValue(ADDRESS_CITY,"Kyiv")
+//                .enterValue(ADDRESS_POSTAL_CODE,"01601")
+//                .enterValue(ADDRESS_MOBILR_PHONE,"+80934655725")
+//                .enterValue(ADDRESS_ALIAS_ADDRESS,"64/13, Volodymyrska Street")
+
+        authPage.clickSubmitButton(ADDRESS_REGISTER);
         WebElement dynamicElement = (new WebDriverWait(driver, 10))
                 .until(ExpectedConditions.presenceOfElementLocated(ALLERT));
         Assert.assertTrue(driver.findElement(ALLERT).getText().contains("There are 3 errors"));
@@ -74,17 +71,7 @@ public class AuthenticationCreate {
         driver.close();
     }
 
-//        driver.navigate().to("http://automationpractice.com/index.php?controller=authentication&back=my-account");
-//        ((ChromeDriver) driver).findElementById("email_create").sendKeys("vinnipuh+1@g.com");
-//        WebDriverWait wait = new WebDriverWait(driver, 20);
-//        ((ChromeDriver) driver).findElementById("SubmitCreate").click();
-//        wait.until(ExpectedConditions.visibilityOf(((ChromeDriver) driver).findElementByXPath("//*[@class='page-heading']")));
-//
-//        System.out.println ( ((ChromeDriver) driver).findElementByXPath("//*[@class='page-heading']").getText()+"  find it");
-//
-//
-//        driver.close();
-//    }
+
 
 
 
